114e371d3ec6124cc51a4d026a095e73725737d4
dnoc <drew.nocton@joinroot.com>
2021-01-07 14:25:25 -0500

    Update project settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,60 +1,57 @@
+const pkg = require('./package');
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  'plugins': [
+    'react',
+    'react-hooks',
+  ],
+  'settings': {
+    'react': {
+      'version': pkg.dependencies.react,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    'rules': {
-        'no-prototype-builtins': 'off',
-        'require-atomic-updates': 'off',
-        'no-async-promise-executor': 'off',
-        'react/jsx-no-bind': ['warn', {
-          'ignoreRefs': false,
-          'allowArrowFunctions': true,
-          'allowBind': false,
-        }],
-        'react/jsx-curly-brace-presence': ['error', {
-          'props': 'always',
-          'children': 'ignore',
-        }],
-        'react/jsx-sort-props': ['error', {
-          'callbacksLast': false,
-          'shorthandFirst': true,
-          'ignoreCase': true,
-          'noSortAlphabetically': false,
-          'reservedFirst': false,
-        }],
-        'root/redux-actions-return-objects': ['error', {
-          'whitelist': reduxActionsReturnObjectsWhitelist,
-        }],
-        'root/valid-test-filenames': [
-          'error',
-          {
-            testSuffix: '-test.js',
-            whitelist: [
-              'test/setup-tests.js',
-              'test/support/integration-helper/deep-links.js',
-            ],
-          },
-        ],
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'error',
+  },
+  'extends': [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:import/warnings',
+      'plugin:import/errors',
+  ],
+  'globals': {
+      'Atomics': 'readonly',
+      'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+      'ecmaFeatures': {
+          'jsx': true
       },
+      'ecmaVersion': 2018,
+      'sourceType': 'module'
+  },
+  'rules': {
+    'indent': ['error', 2],
+    'no-prototype-builtins': 'off',
+    'no-multi-spaces': ['error'],
+    'require-atomic-updates': 'off',
+    'semi': ['error', 'always'],
+    'no-async-promise-executor': 'off',
+    'quotes': ['error', 'single'],
+    'react/jsx-no-bind': ['warn', {
+      'ignoreRefs': false,
+      'allowArrowFunctions': true,
+      'allowBind': false,
+    }],
+    'react/jsx-curly-brace-presence': ['error', {
+      'props': 'always',
+      'children': 'ignore',
+    }],
+    'react/jsx-sort-props': ['error', {
+      'callbacksLast': false,
+      'shorthandFirst': true,
+      'ignoreCase': true,
+      'noSortAlphabetically': false,
+      'reservedFirst': false,
+    }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+  },
 };
